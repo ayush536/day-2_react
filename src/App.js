7833ae7475cf80bdab133e07b8233b9192235e30
ayush536 <82461982+ayush536@users.noreply.github.com>
2022-03-06 18:15:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+import { Counter } from "./Components/Counter";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
